11c05ff087d4623b0ec5c03cabc216ced2d52d72
Marcelo Longen <marcelolongen@gmail.com>
2018-07-07 12:58:01 -0700

    remove blocks that are not onscreen / add basic hit logic

--- app/src/main/java/com/example/marcelolongen/snakeblock/Block.java
+++ app/src/main/java/com/example/marcelolongen/snakeblock/Block.java
@@ -10,9 +10,14 @@ public class Block {
     public Block(int left) {
         this.left = left;
         this.right = left + 150;
-        this.bottom = 0;
-        this.top = 150;
+        this.bottom = 150;
+        this.top = 0;
     }
 
-
+    public Block(int left, int right, int top, int bottom) {
+        this.left = left;
+        this.right = right;
+        this.top = top;
+        this.bottom = bottom;
+    }
 }
--- app/src/main/java/com/example/marcelolongen/snakeblock/MainCanvas.java
+++ app/src/main/java/com/example/marcelolongen/snakeblock/MainCanvas.java
@@ -6,15 +6,15 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainCanvas extends View {
-    private ArrayList<Paint> colors = new ArrayList<>();
     private ArrayList<Rect> blocks = new ArrayList<>();
     private int animationTick = 0;
-    private static final int DROPS_PER_SEC = 2;
     int[] blockPosition = new int[5];
+    int velocity = 5;
 
 
     public MainCanvas(Context context, AttributeSet attrs) {
@@ -26,14 +26,21 @@ public class MainCanvas extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         int area = canvas.getWidth() / 5;
+        final int height = canvas.getHeight();
+
+        Paint blocksColor = new Paint();
+        blocksColor.setARGB(255, 66, 155, 244);
 
         blockPosition[0] = 0;
         blockPosition[1] = area;
         blockPosition[2] =  area + area;
         blockPosition[3] = area + area + area;
         blockPosition[4] = area + area + area + area;
-            if (animationTick % 30 == 0) {
-                System.out.println("tick");
+
+        Block target = new Block(canvas.getWidth()/2 - 25, canvas.getWidth() / 2 + 25,canvas.getHeight() - 100, canvas.getHeight() -50);
+        Rect targetRect = new Rect(target.left, target.top, target.right, target.bottom);
+
+            if (animationTick % 50 == 0) {
                 int random = (int) (Math.random() * 4);
                 if (random == 0) {
                     drawOne();
@@ -48,24 +55,32 @@ public class MainCanvas extends View {
         }
 
         for (int i = 0; i < blocks.size(); i++) {
-            blocks.get(i).bottom += 10;
-            blocks.get(i).top += 10;
-            canvas.drawRect(blocks.get(i), colors.get(i));
-
+            blocks.get(i).bottom += velocity;
+            blocks.get(i).top += velocity;
+            canvas.drawRect(blocks.get(i), blocksColor);
+            checkIfHit(target);
 
         }
+        Paint randomTarg = new Paint();
+        randomTarg.setARGB(255, 255, 0, 0);
+        canvas.drawRect(targetRect,randomTarg);
 
+        checkIfBlockOnScreen(height);
         animationTick++;
 
+    }
+
+    private void checkIfBlockOnScreen(int height) {
+        for (int i = 0; i < blocks.size(); i++){
+            if (blocks.get(i).bottom > height) {
+                blocks.remove(this.blocks.get(i));
+            }
+        }
     }
 
     public void drawOne() {
         Block block = new Block(blockPosition[(int) (Math.random() * 5)]);
         Rect rectangle = new Rect(block.left, block.top, block.right, block.bottom);
-
-        Paint random = new Paint();
-        random.setARGB(255, ((int) (Math.random() * 255) + 2), ((int) (Math.random() * 255) + 2), ((int) (Math.random() * 255) + 2));
-        colors.add(random);
 
         blocks.add(rectangle);
     }
@@ -78,11 +93,6 @@ public class MainCanvas extends View {
         Block block2 = new Block(blockPosition[secondBlock]);
         Rect rectangle1 = new Rect(block1.left, block1.top, block1.right, block1.bottom);
         Rect rectangle2 = new Rect(block2.left, block2.top, block2.right, block2.bottom);
-
-        Paint random = new Paint();
-        random.setARGB(255, ((int) (Math.random() * 255) + 2), ((int) (Math.random() * 255) + 2), ((int) (Math.random() * 255) + 2));
-        colors.add(random);
-        colors.add(random);
 
         blocks.add(rectangle1);
         blocks.add(rectangle2);
@@ -101,11 +111,6 @@ public class MainCanvas extends View {
         Rect rectangle2 = new Rect(block2.left, block2.top, block2.right, block2.bottom);
         Rect rectangle3 = new Rect(block3.left, block3.top, block3.right, block3.bottom);
 
-        Paint random = new Paint();
-        random.setARGB(255, ((int) (Math.random() * 255) + 2), ((int) (Math.random() * 255) + 2), ((int) (Math.random() * 255) + 2));
-        colors.add(random);
-        colors.add(random);
-        colors.add(random);
 
         blocks.add(rectangle1);
         blocks.add(rectangle2);
@@ -127,17 +132,28 @@ public class MainCanvas extends View {
         Rect rectangle3 = new Rect(block3.left, block3.top, block3.right, block3.bottom);
         Rect rectangle4 = new Rect(block4.left, block4.top, block4.right, block4.bottom);
 
-        Paint random = new Paint();
-        random.setARGB(255, ((int) (Math.random() * 255) + 2), ((int) (Math.random() * 255) + 2), ((int) (Math.random() * 255) + 2));
-        colors.add(random);
-        colors.add(random);
-        colors.add(random);
-        colors.add(random);
-
         blocks.add(rectangle1);
         blocks.add(rectangle2);
         blocks.add(rectangle3);
         blocks.add(rectangle4);
     }
+
+
+    public void checkIfHit(Block target) {
+        for (int i = 0; i < blocks.size(); i++) {
+
+                for (int j = 0; j < velocity;j++) {
+                    if (target.top + j == blocks.get(i).bottom) {
+                        if (blocks.get(i).left < target.left && target.left < blocks.get(i).right || blocks.get(i).right < target.right && target.right < blocks.get(i).left) {
+                            Toast.makeText(getContext(), "hit", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                    }
+                }
+
+
+         }
+    }
+
 
 }
