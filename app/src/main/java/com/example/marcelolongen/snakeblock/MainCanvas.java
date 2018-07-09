4c11755441f7fec66c0f9ab70ddde0225a2b5b8a
Marcelo Longen <marcelolongen@gmail.com>
2018-07-09 16:26:44 -0700

    improve performance / bug fixes

--- app/src/main/java/com/example/marcelolongen/snakeblock/MainCanvas.java
+++ app/src/main/java/com/example/marcelolongen/snakeblock/MainCanvas.java
@@ -1,5 +1,6 @@
 package com.example.marcelolongen.snakeblock;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -19,17 +20,33 @@ public class MainCanvas extends View {
     private ArrayList<Rect> blocks = new ArrayList<>();
     private int animationTick = 0;
     int[] blockPosition = new int[5];
-    int velocity = 10;
+    int velocity = 20;
     int left = 0;
     int left2 = 0;
     int left3 = 0;
     int left4 = 0;
     int area;
     int count = 0;
-    int lives = 4;
-    int width = getWidth();
+    int lives = 1;
+    private int width = 0;
+    private int height = 0;
     private boolean hasStarted = false;
     private boolean moving = false;
+
+
+    private int skipHit = 0;
+
+    private Bitmap character1_left = BitmapFactory.decodeResource(getResources(), R.drawable.character_left);
+    private Bitmap character1_right = BitmapFactory.decodeResource(getResources(), R.drawable.character_right);
+    private Bitmap character2_left = BitmapFactory.decodeResource(getResources(), R.drawable.character2_left);
+    private Bitmap character2_right = BitmapFactory.decodeResource(getResources(), R.drawable.character2_right);
+    private Bitmap character3_left = BitmapFactory.decodeResource(getResources(), R.drawable.character3_left);
+    private Bitmap character3_right = BitmapFactory.decodeResource(getResources(), R.drawable.character3_right);
+    private Bitmap character4_left = BitmapFactory.decodeResource(getResources(), R.drawable.character4_left);
+    private Bitmap character4_right = BitmapFactory.decodeResource(getResources(), R.drawable.character4_right);
+    private Bitmap block = BitmapFactory.decodeResource(getResources(), R.drawable.brick);
+    private MotionEvent event;
+
 
     public MainCanvas(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -41,12 +58,12 @@ public class MainCanvas extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         area = canvas.getWidth() / 5;
-        int height = canvas.getHeight();
+        height = canvas.getHeight();
         width = canvas.getWidth();
-        Paint blocksColor = new Paint();
-        Paint red = new Paint();
+        @SuppressLint("DrawAllocation") Paint blocksColor = new Paint();
+        @SuppressLint("DrawAllocation") Paint red = new Paint();
         red.setARGB(255, 255, 0, 0);
-        red.setTextSize(40);
+        red.setTextSize(60);
         red.setTypeface(Typeface.MONOSPACE);
         canvas.drawText("Score " + count , width / 2 - 100, 400, red);
         canvas.drawText("Speed " + velocity , width / 2 - 100, 600, red);
@@ -58,26 +75,41 @@ public class MainCanvas extends View {
         blockPosition[3] = area + area + area;
         blockPosition[4] = area + area + area + area;
 
+        @SuppressLint("DrawAllocation") Block target = new Block(left,
+                left + (area / 2),canvas.getHeight()-450, canvas.getHeight() - 500 - (blockPosition[1] / 2));
 
-        Block target = new Block(left, left + (area / 2),canvas.getHeight()-450, canvas.getHeight() - 500 - (blockPosition[1] / 2));
-        Rect targetRect = new Rect(left, target.top, target.right, target.bottom);
 
 
         drawCharacters(canvas);
 
+        if (skipHit > 0) {
+            skipHit--;
+        }
 
 
-        if (animationTick % 30 == 0) {
-            int random = (int) (Math.random() * 5);
-            for (int i = 0; i < random; i ++) {
-                dropBlock();
+        if (velocity < 20) {
+            if (animationTick % 40 == 0) {
+                int random = (int) (Math.random() * 5);
+                for (int i = 0; i < random; i ++) {
+                    dropBlock();
+                }
+            }
+        } else {
+            if (animationTick % 20 == 0) {
+                int random = (int) (Math.random() * 5);
+                for (int i = 0; i < random; i ++) {
+                        dropBlock();
+
+
+                }
             }
         }
+
 
         for (int i = 0; i < blocks.size(); i++) {
             blocks.get(i).bottom += velocity;
             blocks.get(i).top += velocity;
-            canvas.drawRect(blocks.get(i), blocksColor);
+            canvas.drawBitmap(block, null, blocks.get(i), red);
             checkIfHit(target);
 
         }
@@ -88,8 +120,11 @@ public class MainCanvas extends View {
 
         checkIfBlockOnScreen(height);
         animationTick++;
+        if (animationTick % 1000 == 0) {
+            lives++;
 
-        if (animationTick % 50 == 0) {
+        }
+        if (animationTick % 100 == 0 && velocity < 30) {
             velocity++;
         }
         count++;
@@ -103,41 +138,31 @@ public class MainCanvas extends View {
         if (animationTick % 30 == 0) {
             moving = false;
         }
-
         if (moving) {
-            Bitmap character = BitmapFactory.decodeResource(getResources(), R.drawable.character_left);
-            canvas.drawBitmap(character, left, canvas.getHeight()-500, null);
+            canvas.drawBitmap(character1_left, left, canvas.getHeight()-500, null);
         } else {
-            Bitmap character = BitmapFactory.decodeResource(getResources(), R.drawable.character_right);
-            canvas.drawBitmap(character, left, canvas.getHeight()-500, null);
-        }
 
+            canvas.drawBitmap(character1_right, left, canvas.getHeight()-500, null);
+        }
         if (lives > 1) {
             if (moving) {
-                Bitmap character = BitmapFactory.decodeResource(getResources(), R.drawable.character2_right);
-                canvas.drawBitmap(character, left2, canvas.getHeight()-400, null);
+                canvas.drawBitmap(character2_left, left2, canvas.getHeight()-400, null);
             } else {
-                Bitmap character = BitmapFactory.decodeResource(getResources(), R.drawable.character2_left);
-                canvas.drawBitmap(character, left2, canvas.getHeight()-400, null);
+                canvas.drawBitmap(character2_right, left2, canvas.getHeight()-400, null);
             }
-
         }
         if (lives > 2) {
             if (moving) {
-                Bitmap character = BitmapFactory.decodeResource(getResources(), R.drawable.character3_left);
-                canvas.drawBitmap(character, left3, canvas.getHeight()-300, null);
+                canvas.drawBitmap(character3_left, left3, canvas.getHeight()-300, null);
             } else {
-                Bitmap character = BitmapFactory.decodeResource(getResources(), R.drawable.character3_right);
-                canvas.drawBitmap(character, left3, canvas.getHeight()-300, null);
+                canvas.drawBitmap(character3_right, left3, canvas.getHeight()-300, null);
             }
         }
         if (lives > 3) {
             if (moving) {
-                Bitmap character = BitmapFactory.decodeResource(getResources(), R.drawable.character4_left);
-                canvas.drawBitmap(character, left4, canvas.getHeight()-200, null);
+                canvas.drawBitmap(character4_left, left4, canvas.getHeight()-200, null);
             } else {
-                Bitmap character = BitmapFactory.decodeResource(getResources(), R.drawable.character4_right);
-                canvas.drawBitmap(character, left4, canvas.getHeight()-200, null);
+                canvas.drawBitmap(character4_right, left4, canvas.getHeight()-200, null);
             }
         }
     }
@@ -145,7 +170,7 @@ public class MainCanvas extends View {
     private void checkIfBlockOnScreen(int height) {
         for (int i = 0; i < blocks.size(); i++){
             if (blocks.get(i).bottom > height) {
-
+                blocks.remove(blocks.get(i));
             }
         }
     }
@@ -162,24 +187,36 @@ public class MainCanvas extends View {
     //TODO: Improve crash logic, it is not perfect
     public void checkIfHit(Block target) {
         for (int i = 0; i < blocks.size(); i++) {
+            if (blocks.get(i).bottom > height / 2) {
                 for (int j = 0; j < velocity;j++) {
-                    if (target.top - j == blocks.get(i).top) {
+                    if (target.top - j == blocks.get(i).top && skipHit == 0) {
                         if (blocks.get(i).left < target.left && target.left < blocks.get(i).right || blocks.get(i).right < target.right && target.right < blocks.get(i).left) {
+                            if (lives > 1){
+                                skipHit +=velocity;
+                                lives--;
 
-                            Intent intent = new Intent(getRootView().getContext(), Finish.class);
-                            intent.putExtra("score", count);
-                            getContext().startActivity(intent);
-                            break;
+                            } else {
+                                Intent intent = new Intent(getRootView().getContext(), Finish.class);
+                                intent.putExtra("score", count);
+                                getContext().startActivity(intent);
+                                break;
+                            }
+
+
                         }
                     }
                 }
+            }
+
 
 
          }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        this.event = event;
         float  pos = event.getX();
         switch (event.getAction()) {
             case MotionEvent.ACTION_MOVE: {
@@ -198,11 +235,11 @@ public class MainCanvas extends View {
                     left3 -= width/72 + velocity;
                     left4 -= width/68 + velocity;
                 }
-                invalidate();
                 break;
             }
 
         }
         return true;
     }
+
 }
