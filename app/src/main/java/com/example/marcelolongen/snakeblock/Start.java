f7bf4373eef4edf9fd179b108876e299fe0cdd2f
Marcelo Longen <marcelolongen@gmail.com>
2018-07-11 09:31:22 -0700

    add start activity

---
+++ app/src/main/java/com/example/marcelolongen/snakeblock/Start.java
@@ -0,0 +1,22 @@
+package com.example.marcelolongen.snakeblock;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
